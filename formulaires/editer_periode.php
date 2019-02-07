98ccc7183b58b2bf499d6106ba1abc58a1e95101
abelass <rainer@websimple.be>
2019-02-07 15:46:02 +0100

    convertir arrays

--- formulaires/editer_periode.php
+++ formulaires/editer_periode.php
@@ -19,7 +19,7 @@ include_spip('inc/editer');
 function formulaires_editer_periode_saisies_dist() {
 
 	// Les jours de la semaine
-	$jours_semaines = array();
+	$jours_semaines = [];
 	for ($i = 0; $i < 7; $i++) {
 		$jour = $i + 1;
 		$jours_semaines[] = _T('spip:date_jour_' . $jour);
@@ -40,64 +40,64 @@ function formulaires_editer_periode_saisies_dist() {
 	}
 
 
-	$operateurs = array(
+	$operateurs = [
 		htmlspecialchars('==') => '==',
 		htmlspecialchars('!=') => '!=',
 		htmlspecialchars('<') => '<',
 		htmlspecialchars('<=') => '<=',
 		htmlspecialchars('>') => '>',
 		htmlspecialchars('>=') => '>=',
-	);
+	];
 
-	return array(
-		array(
+	return [
+		[
 			'saisie' => 'input',
-			'options' => array(
+			'options' => [
 				'nom' => 'titre',
 				'obligatoire' => 'oui',
 				'datas' => $statuts,
 				'cacher_option_intro' => 'on',
 				'label' => _T('ecrire:info_titre'),
-			)
-		),
-		array(
+			]
+		],
+		[
 			'saisie' => 'textarea',
-			'options' => array(
+			'options' => [
 				'nom' => 'descriptif',
 				'label' => _T('ecrire:info_descriptif'),
 				'conteneur_class' => 'pleine_largeur',
 				'class' => 'inserer_barre_edition',
 				'rows' => 4
-			)
-		),
-		array(
+			]
+		],
+		[
 			'saisie' => 'radio',
-			'options' => array(
+			'options' => [
 				'obligatoire' => 'oui',
 				'nom' => 'type',
 				'label' => _T('periode:champ_type_label'),
-				'data' => array(
+				'data' => [
 					'date' => _T('periode:type_date'),
 					'jour_semaine' => _T('periode:type_jour_semaine'),
 					//'jour_nombre' => _T('po_periode:type_jour_nombre'),
-				),
+				],
 				'defaut' => 'date',
-			),
-		),
-		array(
+			],
+		],
+		[
 			'saisie' => 'selection',
-			'options' => array(
+			'options' => [
 				'nom' => 'criteres',
 				'label' => _T('periode:champ_criteres_label'),
 				'obligatoire' => 'oui',
-				'data' => array(
+				'data' => [
 					'coincide' => _T('periode:choix_coincide_label'),
 					'exclu' => _T('periode:choix_exclu_label'),
 					//'specifique' => _T('po_periode:choix_specifique_label'),
-				),
+				],
 				'defaut' => 'coincide',
-			)
-		),
+			]
+		],
 		/*array(
 			'saisie' => 'selection',
 			'options' => array(
@@ -107,9 +107,9 @@ function formulaires_editer_periode_saisies_dist() {
 				'afficher_si' => '(@type@ == "date" || @type@ == "jour_nombre") && @criteres@ == "specifique"',
 			)
 		),*/
-		array(
+		[
 			'saisie' => 'oui_non',
-			'options' => array(
+			'options' => [
 				'obligatoire' => 'oui',
 				'nom' => 'date_complete',
 				'label' => _T('periode:champ_date_complete_label'),
@@ -117,16 +117,16 @@ function formulaires_editer_periode_saisies_dist() {
 				'valeur_non' => 'non',
 				'afficher_si' => '@type@ == "date"',
 				'defaut' => 'oui',
-			),
-		),
-		array(
+			],
+		],
+		[
 			'saisie' => 'date',
-			'options' => array(
+			'options' => [
 				'nom' => 'date_debut',
 				'label' => _T('dates_outils:champ_date_debut_label'),
 				'afficher_si' => '@type@ == "date" && @date_complete@ != "non"',
-			)
-		),
+			]
+		],
 		/*array(
 			'saisie' => 'selection',
 			'options' => array(
@@ -136,22 +136,22 @@ function formulaires_editer_periode_saisies_dist() {
 				'afficher_si' => '@type@ == "date" && @criteres@ == "specifique"',
 			)
 		),*/
-		array(
+		[
 			'saisie' => 'date',
-			'options' => array(
+			'options' => [
 				'nom' => 'date_fin',
 				'label' => _T('dates_outils:champ_date_fin_label'),
 				'afficher_si' => '@type@ == "date" && @date_complete@ == "oui"',
-			)
-		),
+			]
+		],
 		[
 			'saisie' => 'fieldset',
-			'options' => array(
+			'options' => [
 				'nom' => 'date_debut_selection',
 				'label' => _T('dates_outils:champ_date_debut_label'),
 				'afficher_si' => '@type@ == "date" && @date_complete@ == "non"',
 
-			),
+			],
 			'saisies' => [
 				[
 					'saisie' => 'selection',
@@ -182,11 +182,11 @@ function formulaires_editer_periode_saisies_dist() {
 		],
 		[
 			'saisie' => 'fieldset',
-			'options' => array(
+			'options' => [
 				'nom' => 'date_fin_selection',
 				'label' => _T('dates_outils:champ_date_fin_label'),
 				'afficher_si' => '@type@ == "date" && @date_complete@ == "non"',
-			),
+			],
 			'saisies' => [
 				[
 					'saisie' => 'selection',
@@ -215,33 +215,33 @@ function formulaires_editer_periode_saisies_dist() {
 				],
 			],
 		],
-		array(
+		[
 			'saisie' => 'selection',
-			'options' => array(
+			'options' => [
 				'nom' => 'jour_debut',
 				'label' => _T('periode:champ_jour_debut_label'),
 				'data' => $jours_semaines,
 				'afficher_si' => '@type@ == "jour_semaine"',
-			)
-		),
-		array(
+			]
+		],
+		[
 			'saisie' => 'selection',
-			'options' => array(
+			'options' => [
 				'nom' => 'jour_fin',
 				'label' => _T('periode:champ_jour_fin_label'),
 				'data' => $jours_semaines,
 				'afficher_si' => '@type@ == "jour_semaine"',
-			)
-		),
-		array(
+			]
+		],
+		[
 			'saisie' => 'input',
-			'options' => array(
+			'options' => [
 				'nom' => 'jour_nombre',
 				'label' => _T('periode:champ_jour_nombre_label'),
 				'afficher_si' => '@type@ == "jour_nombre"',
-			)
-		),
-	);
+			]
+		],
+	];
 }
 
 
@@ -263,8 +263,8 @@ function formulaires_editer_periode_saisies_dist() {
  * @return string
  *     Hash du formulaire
  */
-function formulaires_editer_periode_identifier_dist($id_periode = 'new', $retour = '', $lier_trad = 0, $config_fonc = '', $row = array(), $hidden = '') {
-	return serialize(array(intval($id_periode)));
+function formulaires_editer_periode_identifier_dist($id_periode = 'new', $retour = '', $lier_trad = 0, $config_fonc = '', $row = [], $hidden = '') {
+	return serialize([intval($id_periode)]);
 }
 
 /**
@@ -289,7 +289,7 @@ function formulaires_editer_periode_identifier_dist($id_periode = 'new', $retour
  * @return array
  *     Environnement du formulaire
  */
-function formulaires_editer_periode_charger_dist($id_periode = 'new', $retour = '', $lier_trad = 0, $config_fonc = '', $row = array(), $hidden = '') {
+function formulaires_editer_periode_charger_dist($id_periode = 'new', $retour = '', $lier_trad = 0, $config_fonc = '', $row = [], $hidden = '') {
 	$valeurs = formulaires_editer_objet_charger('periode', $id_periode, '', $lier_trad, $retour, $config_fonc, $row, $hidden);
 
 	return $valeurs;
@@ -317,17 +317,17 @@ function formulaires_editer_periode_charger_dist($id_periode = 'new', $retour = 
  * @return array
  *     Tableau des erreurs
  */
-function formulaires_editer_periode_verifier_dist($id_periode = 'new', $retour = '', $lier_trad = 0, $config_fonc = '', $row = array(), $hidden = '') {
-	$erreurs = array();
+function formulaires_editer_periode_verifier_dist($id_periode = 'new', $retour = '', $lier_trad = 0, $config_fonc = '', $row = [], $hidden = '') {
+	$erreurs = [];
 
 	$type = _request('type');
 	$date_complete = _request('date_complete');
 
 	$verifier = charger_fonction('verifier', 'inc');
 
-	foreach (array('date_debut', 'date_fin') AS $champ) {
+	foreach (['date_debut', 'date_fin'] AS $champ) {
 		$normaliser = null;
-		if ($erreur = $verifier(_request($champ), 'date', array('normaliser'=>'datetime'), $normaliser)) {
+		if ($erreur = $verifier(_request($champ), 'date', ['normaliser'=>'datetime'], $normaliser)) {
 			$erreurs[$champ] = $erreur;
 		// si une valeur de normalisation a ete transmis, la prendre.
 		} elseif (!is_null($normaliser)) {
@@ -395,7 +395,7 @@ function formulaires_editer_periode_verifier_dist($id_periode = 'new', $retour =
  * @return array
  *     Retours des traitements
  */
-function formulaires_editer_periode_traiter_dist($id_periode = 'new', $retour = '', $lier_trad = 0, $config_fonc = '', $row = array(), $hidden = '') {
+function formulaires_editer_periode_traiter_dist($id_periode = 'new', $retour = '', $lier_trad = 0, $config_fonc = '', $row = [], $hidden = '') {
 	$retours = formulaires_editer_objet_traiter('periode', $id_periode, '', $lier_trad, $retour, $config_fonc, $row, $hidden);
 	return $retours;
 }
